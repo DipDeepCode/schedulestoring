da753a6af2354478030236d889f1a0b6c6eba6d6
Sergey Kovalev <dip.deep@mail.ru>
2023-10-30 22:28:24 +0300

    init commit

---
+++ src/main/java/ru/ddc/schedulestoring/SchedulestoringApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.schedulestoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulestoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulestoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ddc/schedulestoring/SchedulestoringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.schedulestoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulestoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
